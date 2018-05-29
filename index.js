3e25c741faeb831654a2997028757a1495416f3a
Denis <igehuc.dev@gmail.com>
2018-05-30 01:19:56 +0300

    implemented default stuff

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var os = require('os');
 var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
@@ -6,13 +7,28 @@ var port = 8000;
 
 var app = express();
 
-// Add morgan logger middleware
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+// Add body parser middleware
+app.use(bodyParser.raw({ type: '*/*' }))
 
-// Add simple response middleware
+// Prepare custom tokens
+morgan.token('eol', function (req, res) {
+  return os.EOL;
+});
+morgan.token('req-headers', function (req, res) {
+  return JSON.stringify(req.headers);
+});
+morgan.token('body-text', function (req, res) {
+  return req.body;
+});
+
+// Add morgan logger middleware
+var fmtBlock = `:date[iso] [:remote-addr] - :response-time ms :eol:method :url HTTP/:http-version:eol:req-headers:eol:body-text:eol`
+app.use(morgan(fmtBlock));
+
+// Add empty response middleware
 app.use(function(req, res) {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end('Nice to see you again!');
+  res.writeHead(200);
+  res.end();
 });
 
 // Start the server
